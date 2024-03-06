1457cd5fbde833933e3a3d6ed870362735ae4a38
godsarin <sarinpradhan7@gmail.com>
2024-03-06 11:39:30 +0545

    string added

---
+++ strings.py
@@ -0,0 +1,10 @@
+# #strings is datatype that stores a sequence of charactersbasic operations
+# concatenation 
+# "hello"+ "world"
+# str = "string"
+# stringLength = len(str)
+# print(stringLength)
+
+str = "Apna_College"
+strSlice = str[-5:-1]
+print(strSlice)#llege
